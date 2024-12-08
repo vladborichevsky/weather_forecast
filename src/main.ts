1823676f9566978d19b6b9d68193b36057ddc0be
Vlad <mr.vladinho@mail.ru>
2024-12-08 22:46:41 +0300

    second commit

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import './assets/main.scss'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import App from './App.vue'
 import focus from '@/directives/Vfocus';
 
 const app = createApp(App)
 
 app.directive('focus', focus);
-
-app.use(createPinia())
 
 app.mount('#app')
